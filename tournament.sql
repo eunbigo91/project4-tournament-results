4cda82dc971386a3275d882d19e529b745beb516
Eunbi Go <eunbigo91@gmail.com>
2016-11-05 18:01:44 -0700

    Feat: add create table and view

--- tournament.sql
+++ tournament.sql
@@ -1,9 +1,19 @@
--- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
+DROP DATABASE IF EXISTS tournament;
 
+CREATE DATABASE tournament;
 
+\c tournament;
+
+create table players (player_id serial primary key, name varchar);
+
+create table matches (match_id serial primary key, winner integer references
+  players(player_id), loser integer references players(player_id));
+
+create view rankings as select player_id, name,
+    count(CASE WHEN player_id = winner then 1 END) as wins,
+    count (matches) as matches
+    from players
+    left join matches on players.player_id = matches.winner
+        or players.player_id = matches.loser
+    group by players.player_id
+    order by wins desc;
