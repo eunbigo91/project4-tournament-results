b5dca712f0cdb935fa144b28ef8160c68d603849
Eunbi Go <eunbigo91@gmail.com>
2016-11-05 18:15:29 -0700

    Add: comments and change sql commends to uppercase

--- tournament.sql
+++ tournament.sql
@@ -2,18 +2,27 @@ DROP DATABASE IF EXISTS tournament;
 
 CREATE DATABASE tournament;
 
+-- Connect to the newly created DB
 \c tournament;
 
-create table players (player_id serial primary key, name varchar);
+-- Create table named players
+-- which has player_id as a primary key and name
+CREATE TABLE players (player_id serial PRIMARY KEY, name varchar);
 
-create table matches (match_id serial primary key, winner integer references
-  players(player_id), loser integer references players(player_id));
+-- Create table named matches
+-- which has match_id as a primary key,
+-- winner's player_id and loser' player_id
+CREATE TABLE matches (match_id serial PRIMARY KEY,
+    winner integer REFERENCES players(player_id),
+    loser integer REFERENCES players(player_id));
 
-create view rankings as select player_id, name,
-    count(CASE WHEN player_id = winner then 1 END) as wins,
-    count (matches) as matches
-    from players
-    left join matches on players.player_id = matches.winner
-        or players.player_id = matches.loser
-    group by players.player_id
-    order by wins desc;
+-- Create view named rankings
+-- which contains player_id, name, wins and matches order by wins
+CREATE VIEW rankings AS SELECT player_id, name,
+    count(CASE WHEN player_id = winner then 1 END) AS wins,
+    count (matches) AS matches
+    FROM players
+    LEFT JOIN matches ON players.player_id = matches.winner
+        OR players.player_id = matches.loser
+    GROUP BY players.player_id
+    ORDER BY wins DESC;
